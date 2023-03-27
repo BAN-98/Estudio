2d723d2ac15509b6469f029c59fff0f51dc3f49d
ryshen <ryshen98@outlook.com>
2023-03-27 00:37:11 -0500

    Practica suma con navegador

---
+++ Master en javaScript/javaScript-01/suma.js
@@ -0,0 +1,9 @@
+// entrada de datos
+let a = prompt('ingrese un numero');
+let b = prompt('ingrese un numero');
+
+//operacion
+let suma = a + b;
+
+//salida de datos
+document.write('la suma es: ', a+b);
