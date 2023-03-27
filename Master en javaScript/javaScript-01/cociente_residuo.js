0484a13e77a13116ee58ad471d3bf6d6403cf776
BAN-98 <ryshen98@outlook.com>
2023-03-27 11:58:00 -0500

    Reto de javaScript-Principiante

---
+++ Master en javaScript/javaScript-01/cociente_residuo.js
@@ -0,0 +1,15 @@
+//definir varables 
+let numero_uno, numero_dos,cociente, residuo;
+
+numero_uno = Number(prompt('Ingrese el primero Numero'));
+numero_dos = Number(prompt('Ingrese el segundo Numero'));
+
+//Proceso
+cociente = numero_uno / numero_dos;
+residuo = numero_uno % numero_dos;
+
+document.write('<h1>El cociente es: ' + cociente + ' y el residuo es: ' + residuo + '<h1>');
+
+//Salida de Datos
+
+
